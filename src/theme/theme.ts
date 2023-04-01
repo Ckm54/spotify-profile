d869366e3636aa25dd3e9cbcbccb955522a8002d
collo <mutaic84@gmail.com>
2023-04-01 16:39:20 +0300

    :hammer: update components

---
+++ src/theme/theme.ts
@@ -0,0 +1,19 @@
+const colors = {
+  brand: {
+    900: "",
+    800: "",
+    700: "",
+  },
+};
+
+const styles = {
+  colors,
+  global: () => ({
+    body: {
+      bg: '#01110A',
+      color: '#fff'
+    }
+  })
+}
+
+export { styles };
