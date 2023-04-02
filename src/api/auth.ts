568fe96bb477301835cdfc3a076bf1ce5c619529
collo <mutaic84@gmail.com>
2023-04-02 10:18:02 +0300

    :hammer: update get access token function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -40,8 +40,26 @@ const generateCodeChallenge = async (codeVerifier: string) => {
     .replace(/=+$/, "");
 };
 
-const getAccessToken = async (clientId: string, code: string) => {
+const getAccessToken = async (clientId: string, code: string): Promise<string> => {
   // gets an access token
+  const verifier = localStorage.getItem("verifier");
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("code_verifier", verifier!);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  });
+
+  const { access_token } = await result.json();
+
+  return access_token;
 };
 
 const fetchProfile = async (token: string): Promise<any> => {
