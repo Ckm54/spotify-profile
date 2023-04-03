67b4cd6f7ee3ce8ecb2c3afd30811f95bd635002
collo <mutaic84@gmail.com>
2023-04-03 16:56:36 +0300

    :hammer: types update

--- types.d.ts
+++ types.d.ts
@@ -65,11 +65,44 @@ export interface Followers {
   total: number;
 }
 
+export interface TopArtists {
+  items:    TopArtistProfile[];
+  total:    number;
+  limit:    number;
+  offset:   number;
+  href:     string;
+  next:     string;
+  previous: null;
+}
+
+export interface TopArtistProfile {
+  external_urls: ExternalUrls;
+  followers:     Followers;
+  genres:        string[];
+  href:          string;
+  id:            string;
+  images:        Image[];
+  name:          string;
+  popularity:    number;
+  type:          string;
+  uri:           string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export interface Followers {
+  href:  null;
+  total: number;
+}
+
 export interface Image {
   height: number;
   url:    string;
   width:  number;
 }
+
 
 export enum Type {
   Artist = "artist",
