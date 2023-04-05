3decdc8fbd819e1ea1eface1a503cff7bec082ad
collo <mutaic84@gmail.com>
2023-04-05 14:47:00 +0300

    :hammer: display artist's albums

---
+++ src/components/ArtistProfilePage/ArtistAlbums.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { getArtistAlbums } from "../../api/apiCalls";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Album, ArtistAlbumsType } from "../../../types";
+import { Box, Flex, Grid, Text } from "@chakra-ui/react";
+import AlbumCard from "../../common/AlbumCard";
+
+type Props = {};
+
+const ArtistAlbums = (props: Props) => {
+  const { id } = useParams();
+  const [artistAlbums, setArtistAlbums] = React.useState<ArtistAlbumsType>();
+
+  const { isLoading } = useQuery(
+    ["getArtistAlbums", id],
+    () => getArtistAlbums(id!, "album"),
+    {
+      onSuccess: (data: ArtistAlbumsType) => {
+        setArtistAlbums(data);
+        console.log(artistAlbums?.total);
+      },
+      enabled: !!id,
+    }
+  );
+  return (
+    <>
+      {artistAlbums?.total === 0 ? (
+        <React.Fragment />
+      ) : (
+        <Box maxW={{xl: "70%"}}>
+          <Flex justifyContent={"space-between"} alignItems="center" px={5}>
+            <Text color={"#fff"} fontWeight={600} mb={8} mt={{xl: 4}}>
+              Top Albums
+            </Text>
+            {/* <Text
+              color="brand.600"
+              cursor="pointer"
+              _hover={{ color: "#fff" }}
+              onClick={() => {}}
+            >
+              View all
+            </Text> */}
+          </Flex>
+
+          {isLoading ? (
+            <Text>Loading...</Text>
+          ) : (
+            <Grid templateColumns={{base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)', xl: 'repeat(6, 1fr)'}}>
+              {artistAlbums?.items.map((album: Album, index: number) => (
+                index < 6 && <AlbumCard albumInfo={album} />
+              ))}
+            </Grid>
+          )}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ArtistAlbums;
