ecf107bd2bb39dbed44323868f638f226f6b4d24
collo <mutaic84@gmail.com>
2023-04-10 19:56:30 +0300

    :hammer: update track audio features page

--- src/components/TrackDetailsPage/index.tsx
+++ src/components/TrackDetailsPage/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom'
+import { getTrackAudioFeatures, getTrackDetails } from '../../api/apiCalls';
+import { Box, Text } from '@chakra-ui/react';
+import { AudioFeature, TrackInfoResponseType } from '../../../types';
 
 type TrackDetailsPageProps = {}
 
 const TrackDetailsPage = (props: TrackDetailsPageProps) => {
+  const [trackDetails, setTrackDetails] = React.useState<TrackInfoResponseType>();
+  const [trackAudioFeatures, setTrackAudioFeatures] = React.useState<AudioFeature>();
+  const [isDetailsFetched, setIsDetailsFetched] = React.useState<boolean>(false);
+
+  const { id } = useParams();
+  const { isLoading } = useQuery(['getTrackInfo', id], () => getTrackDetails(id!), {
+    onSuccess: (data: TrackInfoResponseType) => {
+      setTrackDetails(data);
+      setIsDetailsFetched(true);
+    },
+    enabled: !!id,
+  });
+
+  const {} = useQuery(['getTrackAudioFeatures', id], () => getTrackAudioFeatures(id!), {
+    onSuccess: (data: AudioFeature) => {
+      setTrackAudioFeatures(data);
+    },
+    enabled: isDetailsFetched && !!id
+  })
+
   return (
-    <div>TrackDetailsPage</div>
+    <>
+    {isLoading ? <Text>Loading...</Text> : <Box>Track Details page</Box>}
+    </>
   )
 }
 
